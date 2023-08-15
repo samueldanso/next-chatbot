075c6017828f5ebca1da7f22a87ca75380ffd28d
samueldanso <me.samueldanso@gmail.com>
2023-08-15 15:45:25 +0000

    launch chatbot 1.0

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next Chatbot',
+  description: 'OpenAI chatbot clone by create next app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className='h-full text-white'>
+        {children}
+        </main>
+        </body>
+    </html> 
+  )
+}
